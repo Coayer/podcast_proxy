97c6995bd1e0ea8c6da2e7678f7b30fe48c1bf04
copeland <copeyroyall@mac.com>
2024-12-31 00:01:06 +0000

    Add YouTube streaming and refactoring

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,5 +1,3 @@
-import logging
-
 import magic
 import validators
 import ipaddress
@@ -39,6 +37,5 @@ def check_file(file_bytes, allowed_mime_types, max_size):
 
     if len(file_bytes) > max_size:
         raise ValueError(
-            f"File size exceeds maximum allowed limit: {
-            len(file_bytes) / 1000000}KB/{max_size / {1000000} }KB"
+            f"File size exceeds maximum allowed limit: {len(file_bytes) / 1000000}KB/{max_size / {1000000} }KB"
         )
